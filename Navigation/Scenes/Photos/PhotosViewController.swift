55f8d3470f47228089bf3fe4789a463fd2be6637
pbartashov <p.bartashov@icloud.com>
2022-06-28 22:44:43 +1000

    Выполнено задание

--- Navigation/Scenes/Photos/PhotosViewController.swift
+++ Navigation/Scenes/Photos/PhotosViewController.swift
@@ -44,7 +44,7 @@ final class PhotosViewController: UIViewController {
         setupLayout()
 
         runTest()
-     }
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = false
